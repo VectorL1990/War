a492b7972b73571d23d3c8df54c0407b7cc51b6f
VectorL1990 <842175664@qq.com>
2021-05-10 11:10:55 +0800

    add scripts

---
+++ Assets/Scripts/MatchSystem/Character/Player.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    //Hp
+    public float Hp = 0;
+
+    //Flag indicated whether player is dead
+    public bool isDead = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/MatchSystem/Management/MatchManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MatchManager : MonoBehaviour
+{
+    enum WinFlag {Team1Win, Team2Win, Normal};
+
+    //Team_1 players
+    public Player[] Team1Players;
+
+    //Team_2 players
+    public Player[] Team2Players;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    WinFlag JudgeWin()
+    {
+        int DeadNb = 0;
+        for (int i = 0; i < Team1Players.Length; i++)
+        {
+            if (Team1Players[i].isDead)
+            {
+                DeadNb += 1;
+            }
+        }
+        if (DeadNb == Team1Players.Length)
+        {
+            return WinFlag.Team1Win;
+        }
+        
+        DeadNb = 0;
+        for (int i = 0; i < Team2Players.Length; i++)
+        {
+            if (Team2Players[i].isDead)
+            {
+                DeadNb += 1;
+            }
+        }
+        if (DeadNb == Team2Players.Length)
+        {
+            return WinFlag.Team2Win;
+        }
+
+        return WinFlag.Normal;
+    }
+}
